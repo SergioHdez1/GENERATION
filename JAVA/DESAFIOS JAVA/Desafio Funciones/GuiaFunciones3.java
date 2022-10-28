1d087eba289ddbb4812c700643c3e35bffb2005e
SergioHdez1 <sergio.hernandez.salgado@gmail.com>
2022-10-27 22:15:24 -0300

    Carpetas Java y JS

---
+++ JAVA/DESAFIOS JAVA/Desafio Funciones/GuiaFunciones3.java
@@ -0,0 +1,27 @@
+import java.io.Console;
+
+/* Escriba una función que, dada una temperatura en grados Celsius, la devuelva en grados
+Fahrenheit. Use su función en un programa que lea una cantidad indeterminada de
+temperaturas en Celsius (que termina cuando se ingresa el valor -999) e imprima en pantalla el
+valor correspondiente en Fahrenheit. */
+public class GuiaFunciones3 {
+    static Float CelsiusAFarenheit (float grados){
+        float farenheit = (grados*1.8f)+32;
+        return farenheit;
+    }
+
+    public static void main(String[] args) {
+        Console console = System.console();
+
+        int interruptor = 0;
+        while (interruptor == 0) {
+            Float grado = Float.parseFloat(console.readLine("Ingrese la temperatura en grados Celsius: \n"));
+            System.out.println("La temperatura en grados farenheit es de: "+CelsiusAFarenheit(grado));
+            if (grado == -999){
+                System.out.println("Finalizado");
+                break;
+            }
+        } 
+    }
+    
+}
